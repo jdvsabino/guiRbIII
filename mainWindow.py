8cd3bfc2b4ca16f659a3e307dec080b513ea57e2
John The Sabin <zambino.ueca@gmail.com>
2018-07-31 10:29:45 +0200

    Added Combo Box and containers for pics.

--- mainWindow.py
+++ mainWindow.py
@@ -1,6 +1,7 @@
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk, GdkPixbuf
+
 
 class mainWindow(Gtk.Window):
 
@@ -15,8 +16,8 @@ class mainWindow(Gtk.Window):
         self.leftBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 12)
         self.mainBox.add(self.leftBox)
         
-        self.startButtonBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 3)
-        self.leftBox.add(self.startButtonBox)
+        self.buttonBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 3)
+        self.leftBox.add(self.buttonBox)
         
         self.camSelectBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 3)
         self.leftBox.add(self.camSelectBox)
@@ -24,16 +25,22 @@ class mainWindow(Gtk.Window):
         self.regionsBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 3)
         self.leftBox.add(self.regionsBox)
 
-        self.infoBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 3)
+        self.infoBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 6)
         self.leftBox.add(self.infoBox)
 
         
-
-        self.button1 = Gtk.Button(label="START / STOP")
-        self.button1.connect("clicked", self.on_button1_clicked)
-        self.startButtonBox.pack_start(self.button1, True, True, 0)
+        ### Buttons, labels and boxes - Left ###
+        self.startButton = Gtk.ToggleButton(label="START")
+        self.startButton.connect("toggled", self.on_startButton_clicked)
+        self.buttonBox.pack_start(self.startButton, True, True, 0)
         
-        self.camSelect = Gtk.Button(label="Cam Select")
+        self.camSelect = Gtk.ComboBoxText()
+        self.camSelect.set_entry_text_column(0)
+        self.camSelect.connect("changed", self.on_camSelect_changed)
+        self.camSelect.append_text("TAndor")
+        self.camSelect.append_text("LAndor")
+        self.camSelect.append_text("VAndor")
+        self.camSelect.append_text("Auto")
         self.camSelectBox.pack_start(self.camSelect, True, True, 0)
 
         self.chooseROI = Gtk.Button(label="Choose ROI")
@@ -41,11 +48,57 @@ class mainWindow(Gtk.Window):
 
         self.chooseBkg = Gtk.Button(label="Choose Bkg.")
         self.regionsBox.pack_start(self.chooseBkg, True, True, 0)
+        
+        self.setManually = Gtk.Button(label="Set Manually")
+        self.regionsBox.pack_start(self.setManually, True, True, 0)
 
-        self.button2 = Gtk.Button(label="Stuff")
-        self.button2.connect("clicked", self.on_button2_clicked)
-        self.mainBox.pack_start(self.button2, True, True, 0)
+        self.infoNetwork = Gtk.Label("Network v0.0")
+        self.infoBox.add(self.infoNetwork)
 
+        self.infoStatus = Gtk.Label("Adwin Status\n")
+        self.infoBox.add(self.infoStatus)
+        
+        self.infoGlobalCounts = Gtk.Label("Global counts: ")
+        self.infoBox.add(self.infoGlobalCounts)
+
+        self.infoPicNum = Gtk.Label("Picture number: ")
+        self.infoBox.add(self.infoPicNum)        
+
+        self.infoScanNum = Gtk.Label("Scan number: ")
+        self.infoBox.add(self.infoScanNum)        
+        
+        # self.button2 = Gtk.Button(label="Stuff")
+        # self.button2.connect("clicked", self.on_button2_clicked)
+        # self.mainBox.pack_start(self.button2, True, True, 0)
+
+        ### Sub Boxes - Right ###
+        self.rightBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 12)
+        self.mainBox.add(self.rightBox)
+
+        self.picZoomedBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 12)
+        self.rightBox.add(self.picZoomedBox)
+
+        self.picGrid = Gtk.Grid()
+        self.picGrid.set_column_spacing(3)
+        self.picGrid.set_row_spacing(3)
+        self.rightBox.add(self.picGrid)
+
+        #pixbuf = GdkPixbuf.gdk_pixbuf_new_from_file("figure_1.png")
+        
+        self.picZoomed = Gtk.Image.new_from_file("figure_1.png")
+        self.picZoomedBox.add(self.picZoomed)
+
+        self.picAtoms = Gtk.Image.new_from_file("figure_2.png")
+        self.picGrid.attach(self.picAtoms, 0, 0, 1, 1)
+
+        self.picNoAtoms = Gtk.Image.new_from_file("figure_3.png")
+        self.picGrid.attach(self.picNoAtoms, 1, 0, 1, 1)
+
+        self.picBkg = Gtk.Image.new_from_file("figure_4.png")
+        self.picGrid.attach(self.picBkg, 0, 1, 1, 1)
+
+        self.picOriginal = Gtk.Image.new_from_file("figure_5.png")
+        self.picGrid.attach(self.picOriginal, 1, 1, 1, 1)  
         
 
 ##############################
@@ -53,13 +106,28 @@ class mainWindow(Gtk.Window):
 ##############################
 
 ### Left Box ###
-    def on_button1_clicked(self, widget):
+    def on_startButton_clicked(self, widget):
+        print(self.startButton.get_active())
+        if self.startButton.get_active():
+            self.startButton.set_label("STOP")
+        else:
+            self.startButton.set_label("START")
         print("Hello")
 
     def on_button2_clicked(self, widget):
         print("Goodbye")
 
+    def on_camSelect_changed(self, widget):
+
+        if widget.get_active_text is "Auto":
+            print("Camera will be selected automatically.")
+        else:
+            print(widget.get_active_text() + " is active!")
+
 win = mainWindow()
+
+#print(dir(win.infoLabel))
+
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
