b65d5eb552475793e2805dc1f554ff719f270464
John The Sabin <zambino.ueca@gmail.com>
2018-07-31 17:41:56 +0200

    =Class to create a window to set values.

---
+++ classes/settingWindows.py
@@ -0,0 +1,18 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gdk, GdkPixbuf
+
+class SettingWindow(Gtk.Window):
+
+    def __init__(self, ownTitle):
+
+        Gtk.Window.__init__(self, title = ownTitle)
+
+        self.mainBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 20)
+        self.add(self.mainBox)
+        
+        self.mainGrid = Gtk.Grid()
+        self.mainBox.pack_start(self.mainGrid, True, True, 0)
+
+        self.setButton = Gtk.Button(label = "Set Values")
+        self.mainBox.pack_start(self.setButton, True, True, 0)
