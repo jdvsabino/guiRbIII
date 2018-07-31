29337f73de96807675441d2bbbcb1330c239650b
John The Sabin <zambino.ueca@gmail.com>
2018-07-31 18:35:04 +0200

    =Imports the needed class correctly and the basic structure works.

--- setRangeWindow.py
+++ setRangeWindow.py
@@ -1,18 +1,23 @@
 import gi
+import sys, os
 gi.require_version('Gtk', '3.0')
+sys.path.append('/home/colin/Desktop/Joao/GUI_RbIII')
+sys.path.append('/home/colin/Desktop/Joao/GUI_RbIII/classes')
+# if __name__ == "__main__" and __package__ is None:
+#     __package__ = "classes.settingWindow"
+
 from gi.repository import Gtk, Gdk, GdkPixbuf
-from .settingWindow import SettingWindow
+from classes.settingWindow import *
 
 
-
-class SetRangeWindow(settingWindow):
+class SetRangeWindow(SettingWindow):
 
     def __init__(self):
 
-        settingWindow.__init__(self, "Set Range")
+        SettingWindow.__init__(self, "Set Range")
 
         self.maxLabel = Gtk.Label("Max.")
-        self.mainGrid.attach(self.mainLabel, 0, 0, 1 ,1)
+        self.mainGrid.attach(self.maxLabel, 0, 0, 1 ,1)
         self.maxEntry = Gtk.Entry()
         self.mainGrid.attach(self.maxEntry, 1, 0, 1 ,1)
 
@@ -20,14 +25,16 @@ class SetRangeWindow(settingWindow):
         self.minLabel = Gtk.Label("Min.")
         self.mainGrid.attach(self.minLabel, 0, 1, 1 ,1)
         self.minEntry = Gtk.Entry()
-        self.mainGrid.attach(self.minEntry, 1, 0, 1 ,1)
+        self.mainGrid.attach(self.minEntry, 1, 1, 1 ,1)
         
 
-win = setRangeWindow()
+win = SetRangeWindow()
 
 #print(dir(win.infoLabel))
 
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
-        
+
+
+
