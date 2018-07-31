4c7c5b974b99c29f3c3183b149372a5047bde842
John The Sabin <zambino.ueca@gmail.com>
2018-07-31 17:00:18 +0200

    =Completed bottom part of the window.

--- plotWindow.py
+++ plotWindow.py
@@ -62,6 +62,73 @@ class plotWindow(Gtk.Window):
         self.saveDataButton = Gtk.Button(label = "Save Data")
         self.plotButtonsBox.pack_start(self.saveDataButton, True, True, 0)
 
+        # Bottom Widgets and boxes
+        self.varGrid = Gtk.Grid()
+        self.varGrid.set_column_spacing(3)
+        self.varGrid.set_row_spacing(9)
+        self.bottomBox.add(self.varGrid)
+
+        self.leftVarBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 3)
+        self.varGrid.attach(self.leftVarBox, 0, 1, 1, 4)
+
+        self.rightVarBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 3)
+        self.varGrid.attach(self.rightVarBox, 1, 1, 1, 4)
+    
+        self.varLabel = Gtk.Label("Data Varables")
+        self.varGrid.attach(self.varLabel, 0, 0, 2, 1)
+
+        # Here we add the boxes and labels for each entry
+        # starting with the left box and then the right one.
+
+        # Var 1
+        self.var1Label = Gtk.Label("Var1")
+        self.leftVarBox.pack_start(self.var1Label, True, True, 0)
+        self.var1Entry = Gtk.Entry()
+        self.leftVarBox.pack_start(self.var1Entry, True, True, 0)
+
+        # Var 2
+        self.var2Label = Gtk.Label("Var2")
+        self.leftVarBox.pack_start(self.var2Label, True, True, 0)
+        self.var2Entry = Gtk.Entry()
+        self.leftVarBox.pack_start(self.var2Entry, True, True, 0)
+
+        # Var 3
+        self.var3Label = Gtk.Label("Var3")
+        self.leftVarBox.pack_start(self.var3Label, True, True, 0)
+        self.var3Entry = Gtk.Entry()
+        self.leftVarBox.pack_start(self.var3Entry, True, True, 0)
+
+        # Var 4
+        self.var4Label = Gtk.Label("Var4")
+        self.leftVarBox.pack_start(self.var4Label, True, True, 0)
+        self.var4Entry = Gtk.Entry()
+        self.leftVarBox.pack_start(self.var4Entry, True, True, 0)
+
+        # Var 5
+        self.var5Label = Gtk.Label("Var5")
+        self.rightVarBox.pack_start(self.var5Label, True, True, 0)
+        self.var5Entry = Gtk.Entry()
+        self.rightVarBox.pack_start(self.var5Entry, True, True, 0)
+
+        # Var 6
+        self.var6Label = Gtk.Label("Var6")
+        self.rightVarBox.pack_start(self.var6Label, True, True, 0)
+        self.var6Entry = Gtk.Entry()
+        self.rightVarBox.pack_start(self.var6Entry, True, True, 0)
+
+        # Var 7
+        self.var7Label = Gtk.Label("Var7")
+        self.rightVarBox.pack_start(self.var7Label, True, True, 0)
+        self.var7Entry = Gtk.Entry()
+        self.rightVarBox.pack_start(self.var7Entry, True, True, 0)
+
+        # Var 8
+        self.var8Label = Gtk.Label("Var8")
+        self.rightVarBox.pack_start(self.var8Label, True, True, 0)
+        self.var8Entry = Gtk.Entry()
+        self.rightVarBox.pack_start(self.var8Entry, True, True, 0)        
+
+        
         
         
 
