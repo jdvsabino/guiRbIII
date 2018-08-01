11bbfb9a95634348b070ec539e9e05d759dd0280
John The Sabin <zambino.ueca@gmail.com>
2018-08-01 12:01:53 +0200

    =Created window to set range manually.

---
+++ setRegionWindow.py
@@ -0,0 +1,73 @@
+import gi
+import sys, os
+gi.require_version('Gtk', '3.0')
+sys.path.append('/home/colin/Desktop/Joao/GUI_RbIII')
+sys.path.append('/home/colin/Desktop/Joao/GUI_RbIII/classes')
+# if __name__ == "__main__" and __package__ is None:
+#     __package__ = "classes.settingWindow"
+
+from gi.repository import Gtk, Gdk, GdkPixbuf
+from classes.settingWindow import *
+
+
+class SetRegionWindow(SettingWindow):
+
+    def __init__(self):
+
+        SettingWindow.__init__(self, "Set Region")
+
+        # Here we choose the position of the label "Up" and set everything
+        # relatively to it. 
+        posUpLabel_col = 0
+        posUpLabel_row = 1
+
+        # We create and position the labels
+        # Label Creation
+        self.upLabel = Gtk.Label("Up")
+        self.downLabel = Gtk.Label("Down")
+        self.leftLabel = Gtk.Label("Left")
+        self.rightLabel = Gtk.Label("Right")
+        self.roiLabel = Gtk.Label("ROI")
+        self.rbcLabel = Gtk.Label("RBC")
+
+        self.mainGrid.attach(self.upLabel, posUpLabel_col, posUpLabel_row, 1 ,1)
+        self.mainGrid.attach(self.downLabel, posUpLabel_col, posUpLabel_row + 1, 1 ,1)
+        self.mainGrid.attach(self.leftLabel, posUpLabel_col, posUpLabel_row + 2, 1 ,1)
+        self.mainGrid.attach(self.rightLabel, posUpLabel_col, posUpLabel_row + 3, 1 ,1)
+        self.mainGrid.attach(self.roiLabel, posUpLabel_col + 1, posUpLabel_row -1, 1 ,1)
+        self.mainGrid.attach(self.rbcLabel, posUpLabel_col + 2, posUpLabel_row -1, 1 ,1)
+
+        # Now the entries are created and positioned
+        # For ROI
+        self.upEntry_roi = Gtk.Entry()
+        self.downEntry_roi = Gtk.Entry()
+        self.leftEntry_roi = Gtk.Entry()
+        self.rightEntry_roi = Gtk.Entry()
+        
+        self.mainGrid.attach(self.upEntry_roi, posUpLabel_col + 1, posUpLabel_row, 1 ,1)
+        self.mainGrid.attach(self.downEntry_roi, posUpLabel_col + 1, posUpLabel_row + 1, 1 ,1)
+        self.mainGrid.attach(self.leftEntry_roi, posUpLabel_col + 1, posUpLabel_row + 2, 1 ,1)
+        self.mainGrid.attach(self.rightEntry_roi, posUpLabel_col + 1, posUpLabel_row + 3, 1 ,1)
+
+        # For RBC
+        self.upEntry_rbc = Gtk.Entry()
+        self.downEntry_rbc = Gtk.Entry()
+        self.leftEntry_rbc = Gtk.Entry()
+        self.rightEntry_rbc = Gtk.Entry()
+        
+        self.mainGrid.attach(self.upEntry_rbc, posUpLabel_col + 2, posUpLabel_row, 1 ,1)
+        self.mainGrid.attach(self.downEntry_rbc, posUpLabel_col + 2, posUpLabel_row + 1, 1 ,1)
+        self.mainGrid.attach(self.leftEntry_rbc, posUpLabel_col + 2, posUpLabel_row + 2, 1 ,1)
+        self.mainGrid.attach(self.rightEntry_rbc, posUpLabel_col + 2, posUpLabel_row + 3, 1 ,1)
+
+
+win = SetRegionWindow()
+
+# #print(dir(win.infoLabel))
+
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
+
+
+
