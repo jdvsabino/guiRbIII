aaa2043768a5b1080bb8f38239c2b2e07085f9ea
John The Sabin <zambino.ueca@gmail.com>
2018-08-03 12:43:22 +0200

    =Adapted code to work with the new window class from 'plotWindow'

--- mainWindow.py
+++ mainWindow.py
@@ -1,9 +1,17 @@
 import gi
-import sys
-sys.path.append('/home/colin/Desktop/Joao/GUI_RbIII')
+import sys, os
 gi.require_version('Gtk', '3.0')
+sys.path.append('/home/colin/Desktop/Joao/GUI_RbIII')
+sys.path.append('/home/colin/Desktop/Joao/GUI_RbIII/classes')
 from gi.repository import Gtk, Gdk, GdkPixbuf
+from setRangeWindow import SetRangeWindow
+import matplotlib.image as mpimg
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_gtk3cairo import FigureCanvasGTK3Cairo as FigureCanvas
+from matplotlib.backends.backend_gtk3 import NavigationToolbar2GTK3 as NavigationToolbar
+from numpy import sin, cos, pi, linspace
 from plotWindow import *
+
 
 class mainWindow(Gtk.Window):
 
@@ -96,27 +104,19 @@ class mainWindow(Gtk.Window):
         self.picZoomed = Gtk.Image.new_from_pixbuf(self.piczoom2)#Gtk.Image.new_from_file("figure_1.tif")
         self.picZoomedBox.add(self.picZoomed)
 
-        # self.picAtoms = GdkPixbuf.Pixbuf.new_from_file_at_scale("figure_2.tif", 300, 300, False)
-        # self.picAtoms = Gtk.Image.new_from_pixbuf(self.picAtoms)
-        # self.picGrid.attach(self.picAtoms, 0, 0, 1, 1)
-        
-        # self.picNoAtoms = GdkPixbuf.Pixbuf.new_from_file_at_scale("figure_3.tif",300,300, False)
-        # self.picNoAtoms = Gtk.Image.new_from_pixbuf(self.picNoAtoms)
-        # self.picGrid.attach(self.picNoAtoms, 1, 0, 1, 1)
-
-        # self.picBkg = GdkPixbuf.Pixbuf.new_from_file_at_scale("figure_4.tif",300,300, False)
-        # self.picBkg = Gtk.Image.new_from_pixbuf(self.picBkg)
-        # self.picGrid.attach(self.picBkg, 0, 1, 1, 1)
-
-        # self.picOriginal = GdkPixbuf.Pixbuf.new_from_file_at_scale("figure_5.tif",300,300, False)
-        # self.picOriginal = Gtk.Image.new_from_pixbuf(self.picOriginal)
-        # self.picGrid.attach(self.picOriginal, 1, 1, 1, 1) 
-        
-
-
         # Plot Window
-        self.plotWin = plotWindow()
+        self.plotWin = newPlotWindow()
         self.plotWin.show_all()
+
+        # Zoom stuff ???
+        self.picOriginal = Gtk.Image()
+        self.zoom = Gtk.GestureDrag.new(self.picOriginal)
+        self.zoom.connect("begin", self.zoomEvent)
+        
+        
+        
+        
+        
     ### CALL BACK FUNCTIONS ###
     def on_setRegionButton_clicked(self, widget):
         from setRegionWindow import SetRegionWindow
@@ -129,6 +129,7 @@ class mainWindow(Gtk.Window):
         self.picAtoms = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename, self.picSize, self.picSize, False)
         self.picAtoms = Gtk.Image.new_from_pixbuf(self.picAtoms)
         self.picGrid.attach(self.picAtoms, 0, 0, 1, 1)
+
 
     def set_picNoAtoms(self, filename):
         self.picNoAtoms = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename, self.picSize, self.picSize, False)
@@ -144,6 +145,12 @@ class mainWindow(Gtk.Window):
         self.picOriginal = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename, self.picSize, self.picSize, False)
         self.picOriginal = Gtk.Image.new_from_pixbuf(self.picOriginal)
         self.picGrid.attach(self.picOriginal, 1, 1, 1, 1)
+        self.zoom = Gtk.GestureLongPress.new(self.picOriginal)
+        self.zoom.connect("pressed", self.zoomEvent)
+
+    def zoomEvent(self, widget):
+        print("Yaaaay")    
+        
 
 
 
@@ -172,11 +179,50 @@ win = mainWindow()
 #print(dir(win.infoLabel))
 
 win.connect("destroy", Gtk.main_quit)
-win.set_picAtoms("figure_2.tif")
-win.set_picNoAtoms("figure_3.tif")
-win.set_picBkg("figure_4.tif")
-win.set_picOriginal("figure_5.tif")
+# win.set_picAtoms("figure_2.tif")
+# win.set_picNoAtoms("figure_3.tif")
+# win.set_picBkg("figure_4.tif")
+# win.set_picOriginal("figure_5.tif")
 
+win.set_picAtoms("manos_na_neve.png")
+win.set_picNoAtoms("manos_na_neve.png")
+win.set_picBkg("manos_na_neve.png")
+win.set_picOriginal("manos_na_neve.png")
 #win.set_resizable(False)
+
+
+# n = 1000
+# xsin = linspace(-pi, pi, n, endpoint=True)
+# xcos = linspace(-pi, pi, n, endpoint=True)
+# ysin = sin(xsin)
+# ycos = cos(xcos)
+
+# sinwave = win.plotWin.ax.plot(xsin, ysin, color='black', label='sin(x)')
+# coswave = win.plotWin.ax.plot(xcos, ycos, color='black', label='cos(x)', linestyle='--')
+
+# win.plotWin.ax.set_xlim(-pi,pi)
+# win.plotWin.ax.set_ylim(-1.2,1.2)
+
+# win.plotWin.ax.fill_between(xsin, 0, ysin, (ysin - 1) > -1, color='blue', alpha=.3)
+# win.plotWin.ax.fill_between(xsin, 0, ysin, (ysin - 1) < -1, color='red',  alpha=.3)
+# win.plotWin.ax.fill_between(xcos, 0, ycos, (ycos - 1) > -1, color='blue', alpha=.3)
+# win.plotWin.ax.fill_between(xcos, 0, ycos, (ycos - 1) < -1, color='red',  alpha=.3)
+
+# win.plotWin.ax.legend(loc='upper left')
+
+# win.plotWin.ax = win.plotWin.fig.gca()
+# win.plotWin.ax.spines['right'].set_color('none')
+# win.plotWin.ax.spines['top'].set_color('none')
+# win.plotWin.ax.xaxis.set_ticks_position('bottom')
+# win.plotWin.ax.spines['bottom'].set_position(('data',0))
+# win.plotWin.ax.yaxis.set_ticks_position('left')
+# win.plotWin.ax.spines['left'].set_position(('data',0))
+# win.plotWin.fig.tight_layout()
+
+# win.plotWin.canvas = FigureCanvas(win.plotWin.fig)
+# toolbar = NavigationToolbar(win.plotWin.canvas, win.plotWin)
+# win.plotWin.plotBox.pack_start(toolbar, False, False, 0)
+
 win.show_all()
 Gtk.main()
+
