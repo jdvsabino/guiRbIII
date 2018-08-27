9943104c278b037a57b47e23b7b1c0b7fc0fd743
John The Sabin <zambino.ueca@gmail.com>
2018-08-27 11:21:40 +0200

    =Added to subclasses. Corrected the arguments given to the Rectangle class.

--- classes/drawRectangle.py
+++ classes/drawRectangle.py
@@ -1,17 +1,45 @@
+from matplotlib.patches import Rectangle
+
 class drawRectangle:
 
     def __init__(self, up, down, left, right):
 
-        self.x = left
-        self.y = up
+        self.ID = None
+        
+        self.x_start = left
+        self.y_start = up
 
-        self.width = right - left
-        self.height = up - down
+        self.x_end = right
+        self.y_end = down
 
-    def rectangleROI():
-        rectangle = Rectangle(self.x,self.y, self.width, self.height, facecolor = "green", alpha = 0.2)
+        self.width = self.x_end -self.x_start
+        self.height = self.y_end - self.y_start
+        
+
+
+    def drawRectangle(self):
+        raise NotImplementedError
+
+
+class roiRectangle(drawRectangle):
+
+    def __init__(self, up=0, down=0, left=0, right=0):
+        drawRectangle.__init__(self, up, down, left, right)
+        self.ID = 0 # Defines object's identity as a ROI recangle
+    
+    def drawRectangle(self):
+        rectangle = Rectangle((self.x_start,self.y_end), self.width, self.height, facecolor = "green", alpha = 0.2)
         return rectangle
 
-    def rectangleRBC():
-        rectangle = Rectangle(self.x,self.y, self.width, self.height, facecolor = "white", alpha = 0.2)
-        return rectangle    
+
+class rbcRectangle(drawRectangle):
+
+    def __init__(self, up=0, down=0, left=0, right=0):
+        drawRectangle.__init__(self, up, down, left, right)
+        self.ID = 1 # Defines object's identity as a RBC recangle
+    
+    def drawRectangle(self):
+        rectangle = Rectangle((self.x_start,self.y_end), self.width, self.height, facecolor = "green", alpha = 0.2)
+        return rectangle
+
+
