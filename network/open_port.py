6ab68d29de1fcec3cee9bc4a405a7fac44ff4eb1
John The Sabin <zambino.ueca@gmail.com>
2018-09-03 15:56:08 +0200

    =Code to open port where input information comes.

--- network/open_port.py
+++ network/open_port.py
@@ -7,7 +7,7 @@ REC_PORT = "8101"
 LOCAL_IP = "192.0.0.123"
 
 def start_comm(local_ip, rec_port_adwin):
-
+    ''' Opens connection to port rec_port_adwin'''
     print("Local IP set to: " + local_ip + ".")
     print("Trying to open and bind socket in port: " + rec_port + ".")
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
@@ -15,4 +15,5 @@ def start_comm(local_ip, rec_port_adwin):
     sock.bind((LOCAL_IP, REC_PORT_ADWIN))
     print(sock)
     print("sucess!")
+    return sock
 
