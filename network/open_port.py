4dcbcb438ebe3dfeccf69cf37a52ffe062b57369
John The Sabin <zambino.ueca@gmail.com>
2018-09-04 13:19:35 +0200

    =Debugged and corrected errors.

--- network/open_port.py
+++ network/open_port.py
@@ -3,16 +3,16 @@ import socket
 import select
 import thread
 
-REC_PORT = "8101"
-LOCAL_IP = "192.0.0.123"
+#REC_PORT = "8101"
+#LOCAL_IP = "192.0.0.123"
 
 def start_comm(local_ip, rec_port_adwin):
     ''' Opens connection to port rec_port_adwin'''
     print("Local IP set to: " + local_ip + ".")
-    print("Trying to open and bind socket in port: " + rec_port + ".")
+    print("Trying to open and bind socket in port: " + rec_port_adwin + ".")
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
     sock.setblocking(0)
-    sock.bind((LOCAL_IP, REC_PORT_ADWIN))
+    sock.bind((local_ip, int(rec_port_adwin)))
     print(sock)
     print("sucess!")
     return sock
