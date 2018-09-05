b1564d4a64c007d0f0770a27ca6aad111140e5eb
John The Sabin <zambino.ueca@gmail.com>
2018-09-05 15:22:26 +0200

    =Corrected a bug and adapted the function to collect data from celcius computers.

--- network/get_data.py
+++ network/get_data.py
@@ -6,24 +6,52 @@ import select
 REC_PORT = "17600"#"8101" 
 LOCAL_IP = "127.0.0.1"#"128.131.60.27"
 
+input_info = Data_Collection() # This is going to be a global variable -  CAREFUL!!
 
+def get_data(local_ip, rec_port_adwin, rec_port_c1, rec_port_c2, rec_port_c3):
+    '''
+    This function is responsible for getting all the data necessary
+    for the program to work. It should be run in a new thread
+    to make synching easier.
+    '''
+    ###---- Defined in the main program
+    global input_info 
 
-def get_data(local_ip, rec_port):
+    ###---- Opening sockets for communication
+    adwin_socket = start_comm(local_ip, rec_port_adwin)
 
-    adwin_socket = start_comm(local_ip, rec_port)
+    #---- Celcius computers
+    c1_socket = start_comm(local_ip, rec_port_c1)
+    c2_socket = start_comm(local_ip, rec_port_c2)
+    c3_socket = start_comm(local_ip, rec_port_c3)    
+
     inputs = [adwin_socket]
-    input_info = Data_Collection()
     
     while True:
         print("Inside while loop!")
-        readable, writable, exceptional = select.select(inputs,[],[]) #check if any port received something
+        readable, writable, exceptional = select.select(inputs,[],[],3) #check if any port received something
         print("Read something...")
         print(readable)
-        for message in readable:
+        for socket in readable:
             data, addr = message.recvfrom(1024)
             print("DATA: " + data)
-            input_info.set_data(data)
 
+            if socket is adwin_socket:
+                input_info.set_data_adwin(data)
+
+            elif socket is c1_socket:
+                input_info.set_data_c1(data)
+
+            elif socket is c2_socket:
+                input_info.set_data_c2(data)
+
+            elif socket is c3_socket:
+                input_info.set_data_c3(data)
+
+            else:
+                print("Error: wierd socket detected!")
+            
+            
 
 print("Started listening to port " + REC_PORT + ", on local machine.")
 get_data(LOCAL_IP, REC_PORT)
