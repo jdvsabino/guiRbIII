02027b697585c8c216d8b6a8038d7b264533767e
John The Sabin <zambino.ueca@gmail.com>
2019-01-15 19:23:55 +0100

    =Corrected bugs and added code to test connection with AdWin. It worked! :D

--- main.py
+++ main.py
@@ -1,30 +1,49 @@
 import sys
-sys.path.append('./network')
-sys.path.append('./analysis')
-sys.path.append('./gui')
+sys.path.append('.\network')
+sys.path.append('.\analysis')
+sys.path.append('.\gui')
 
 import threading, select
-from mainWindow import mainWindow
-from analysis.picManager import PictureManager
+#from gui.mainWindow import mainWindow
+#from analysis.picManager import PictureManager
+
 from network.data_collection import data_collector as  dc
+
 from network.get_data import get_data
 
 
 ###---- MAIN VARIABLES
-REC_PORT_AD = "5005" # Receiving port
-REC_PORT_C1 = "1234"
-REC_PORT_C2 = "1235"
-REC_PORT_C3 = "1236"
+REC_PORT_AD = "8101" # Receiving port
+REC_PORT_C1 = "8103"
+REC_PORT_C2 = "8102"
+REC_PORT_C3 = "8107"
 
-LOCAL_IP = "127.0.0.1"
+LOCAL_IP = "192.0.0.100"
 
 
 
+
+dcbuffer =  "" # Just for testing
 
 
 ###---- Thread to receive data
-t = threading.Thread(get_data(LOCAL_IP, REC_PORT_AD, REC_PORT_C1, REC_PORT_C2, REC_PORT_C3, dc))
+print("Trying to communicate with Adwin...")
+t = threading.Thread(target=get_data,args=((LOCAL_IP, REC_PORT_AD, REC_PORT_C1, REC_PORT_C2, REC_PORT_C3, dc)))
+t.start()
+print("Done!")
 
+
+print(t.is_alive())
+while(1):
+
+    if dc.glob != dcbuffer:
+        print("#####+++++#####")
+        print(str(dc.glob))
+        print("#####+++++#####")
+        dcbuffer = dc.glob
+    
+
+a=input("Press any key to exit...")
 print("BBBB: " + dc.path)
 
 
