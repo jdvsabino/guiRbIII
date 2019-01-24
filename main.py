b6c8bab24ffc90f48aeabf385ada06ef128fa949
John The Sabin <zambino.ueca@gmail.com>
2019-01-24 16:23:26 +0100

    =Minor changes for testing purposes.

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ import threading, select
 from network.data_collection import data_collector as  dc
 
 from network.get_data import get_data
-from analysis.info_manager import InfoManager
+from analysis.infoManager import InfoManager
 
 
 ###---- MAIN VARIABLES
 REC_PORT_AD = "8101" # Receiving port
 REC_PORT_C1 = "8103"
-REC_PORT_C2 = "8102"
+REC_PORT_C2 = "8104"
 REC_PORT_C3 = "8107"
 
-LOCAL_IP = "192.0.0.100"
+LOCAL_IP = "127.0.0.1"
 
 
 
@@ -44,10 +44,10 @@ while(1):
     
     # Holds the program while dc
     # does not receive data from  Adwin.
-    while(dc.glob = -1):
+    while(dc.glob == -1):
         continue
 
-    read_data = dc.receiving != 1 and dc.glob !=
+    read_data = dc.receiving != 1 and dc.glob != info_man.dc.glob
     if read_data:
         print("#####+++++#####")
         info_man.update_data_buffer()
@@ -56,4 +56,4 @@ while(1):
         
     
 
-a=str(input("Press any key to exit..."))
+
