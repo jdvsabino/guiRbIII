09d05999e59998c0b42594bfbe7738d20779c454
John The Sabin <zambino.ueca@gmail.com>
2019-01-27 17:07:22 +0100

    =replaced name of variable 'input_info' to make code clear.

--- network/get_data.py
+++ network/get_data.py
@@ -1,5 +1,5 @@
 from open_port import start_comm
-import data_collection as dc
+from data_collection import data_collector as dc
 import select
 import threading
 
@@ -21,7 +21,7 @@ def get_data(local_ip, rec_port_adwin, rec_port_c1, rec_port_c2, rec_port_c3, in
     c3_socket = start_comm(local_ip, rec_port_c3)    
 
     inputs = [adwin_socket, c1_socket, c2_socket, c3_socket]
-    input_info = dc.data_collector
+    #input_info = dc.data_collector
     
     while 1:
         print("Inside while loop!")
@@ -35,21 +35,22 @@ def get_data(local_ip, rec_port_adwin, rec_port_c1, rec_port_c2, rec_port_c3, in
             print("DATA: " + data)
 
             if socket is adwin_socket:
-                input_info.set_data_adwin(data)
+                dc.set_data_adwin(data)
 
             elif socket is c1_socket:
-                input_info.set_data_c1(data)
+                dc.set_data_c1(data)
 
             elif socket is c2_socket:
-                input_info.set_data_c2(data)
+                dc.set_data_c2(data)
 
             elif socket is c3_socket:
-                input_info.set_data_c3(data)
+                dc.set_data_c3(data)
         
             else:
                 print("Error: wierd socket detected!")
                 if i%2==0:
-                    input_info.path="PAR!"
+                    dc.path="PAR!"
                 else:
-                    input_info.path="IMPAR!"
+                    dc.path="IMPAR!"
+
 
