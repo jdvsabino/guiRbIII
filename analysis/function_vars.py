93fefadddb61084ad8ad4728794fe8bc8f07855b
John The Sabin <zambino.ueca@gmail.com>
2019-01-31 12:01:27 +0100

    =File with functions to compute variables and list of variables to be computed.

---
+++ analysis/function_vars.py
@@ -0,0 +1,16 @@
+def atomnumber(im):
+
+    return self.atom_num
+    
+
+def sigma_H(im):
+
+    print("This is sigma_H! :D")
+    return 0
+
+
+def sigma_V(im):
+
+    print("This is sigma_V! :D")
+    return 0
+
