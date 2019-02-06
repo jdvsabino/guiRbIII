e720fda673ee724655a6814d7d5d090e39d0b1e5
John The Sabin <zambino.ueca@gmail.com>
2019-02-06 18:10:17 +0100

    =Crerated 'copy_flag' to indicate when dc is being copied (by info_manager objects). added 'last_pic' member.

--- network/data_collection.py
+++ network/data_collection.py
@@ -1,8 +1,12 @@
 class Data_Collection():
 
     def __init__(self):
-
-        self.receiving = 0
+        
+        ###--- Flags
+        self.receiving_flag = 0
+        self.copy_flag = 0
+        self.cam_flag = -1
+        
         self.path = ""
         self.file = ""
         self.scan = ""
@@ -12,24 +16,29 @@ class Data_Collection():
         self.T_cam = -1
         self.L_cam = -1
         self.V_cam = -1
-        self.cam_flag = -1
-
+        self.last_pic = -1
+        
         ###---- Chosses the starting point to read data from adwin
         self.adwin_data_start = 5
-        
+    
     def set_data_adwin(self, data):
         ''' 
         Function called when info from adwin computer is recieved.
         Chooses which field the info should be adssigned to.
         field: a string representing the field (example: "PATH", "IMSC", etc.)
         '''
+        
+        ###--- Make sure that info is not being copied
+        while(self.copy_flag == 1):
+            continue
+        
+        self.receiving = 1
         if "STAT_sending" in data:
-            self.receiving = 1
             self.stat_sending()
-            
+        
         elif "STAT_waiting" in data:
             self.stat_waiting()
-            
+        
         elif "PATH" in data:
             self.path = data[self.adwin_data_start:]
             return 1
@@ -59,34 +68,37 @@ class Data_Collection():
             print("Field Not recognized. Check spelling!")
             return 0
         
-
+        
     def st_data_c1(self, data):
         if 'LCAM' in data:
-            self.L_cam = data[self.adwin_data_start:]
+            # self.L_cam = data[self.adwin_data_start:]
+            self.last_pic = = data[self.adwin_data_start:]
             self.cam_flag = 1
             return 1
-
+        
         print("Signal from Celcius 1 recieved but no LCAM info.")
         return 0
 
     def st_data_c2(self, data):
         if 'TCAM' in data:
-            self.T_cam = data[self.adwin_data_start:]
+            #self.T_cam = data[self.adwin_data_start:]
+            self.last_pic = = data[self.adwin_data_start:]
             self.cam_flag = 0
             return 1
-
+        
         print("Signal from Celcius 2 recieved but no TCAM info.")
         return 0
 
     def st_data_c3(self, data):
         if 'VCAM' in data:
-            self.V_cam = data[self.adwin_data_start:]
+            #self.V_cam = data[self.adwin_data_start:]
+            self.last_pic = = data[self.adwin_data_start:]            
             self.cam_flag = 3
             return 1
-
+        
         print("Signal from Celcius 3 recieved but no VCAM info.")
         return 0
-
+    
     
     def stat_sending(self):
         print("Receiving stuff!")
@@ -97,7 +109,7 @@ class Data_Collection():
 data_collector = Data_Collection()
 
 
-####---- Needed paths - TALK TO THOMAS!!!
+####---- Needed paths
 ''' 
 These paths are the soure of the information.
 Where to get the pics, for example.
@@ -105,5 +117,5 @@ Where to get the pics, for example.
 
 LOOP_DATA_DIR_SRC = "G:\\data\\AdWin_v2\\loop-data"
 SCAN_COUNT_FILE_SRC = "G:\\data\\AdWin_v2\\loop-data\\scan_cnt.txt"
-PIC_SRC = "G:\\data\\pics"
+PIC_SRC = "Z:\\data\\pics\\transfer\\LAndor\\" #"G:\\data\\pics"
 # AQ_DATA_SRC = ""
