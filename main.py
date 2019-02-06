12128d14704760ed26e473e9043997999a36e1f0
John The Sabin <zambino.ueca@gmail.com>
2019-02-06 18:33:16 +0100

    =Changes for testing. Doesn't load objects from other file (gives an error).

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import sys
-sys.path.append('.\network')
-sys.path.append('.\analysis')
-sys.path.append('.\gui')
+import os
+curr_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(curr_path + '\\network')
+sys.path.append(curr_path + '\\analysis')
+sys.path.append(curr_path + '\\gui')
 
 import threading, select
 #from gui.mainWindow import mainWindow
 #from analysis.picManager import PictureManager
 
-from network.data_collection import data_collector as  dc
+
 
 from network.get_data import get_data
+a = str(input("Something"))
+from network.data_collection import data_collector as  dc
 from analysis.infoManager import InfoManager
-
+import time # TESTING PURPOSES
 
 ###---- MAIN VARIABLES
 REC_PORT_AD = "8101" # Receiving port
@@ -46,12 +50,18 @@ while(1):
     # does not receive data from  Adwin.
     while(dc.glob == -1):
         continue
+    print("Sleeping...")
+    time.sleep(5)
+    print("Awaken!")
     #print("CURRENT GLOB: " + str(dc.glob))
-    read_data = dc.receiving != 1 and dc.glob != info_man.dc.glob
+    read_data = dc.receiving_flag != 1 and dc.glob != info_man.dc.glob
     if read_data:
+
         print("#####+++++#####")
         info_man.update_data_buffer()
         info_man.update_info(None) # Argument is none for testing purposes
+        print("STATUS: " + str(info_man.status))
+        print("HIST: " + str(info_man.history))
         print("#####+++++#####")
         
     
