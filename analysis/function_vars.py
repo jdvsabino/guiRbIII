48582c9b482b10e6724ee082bbfb857c6c45449a
Joao Sabino <joao.sabino@tuwien.ac.at>
2019-09-02 17:54:49 +0200

    Added functions

--- analysis/function_vars.py
+++ analysis/function_vars.py
@@ -2,7 +2,22 @@ import numpy as np
 def atom_number(im):
 
     return im.atom_num
+
+def pos_h(im):
     
+    if len(im.abs_pic.fit_pars["x"])  == 0:
+        return -1
+    
+    return im.abs_pic.fit_pars["x"][1]
+
+
+def pos_v(im):    
+
+    if len(im.abs_pic.fit_pars["y"])  == 0:
+        return -1
+        
+    return im.abs_pic.fit_pars["y"][1]
+
 
 def sigma_H(im):
 
@@ -29,8 +44,8 @@ def gen_func_dict(var, func):
 
     return func_dict
 
-var_list  = ["Atom Number", "Sigma H", "Sigma V"]
-func_list = [atom_number, sigma_H, sigma_V]
+var_list  = ["Atom Number", "Pos H", "Pos V", "Sigma H", "Sigma V",]
+func_list = [atom_number, pos_h, pos_v, sigma_H, sigma_V]
 
 func_dict = gen_func_dict(var_list, func_list)
 
